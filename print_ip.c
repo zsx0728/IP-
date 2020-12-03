c52a2fc3ad0462d5d3e95508868885a63e932d39
zsx0728 <zsx0728@163.com>
2020-12-03 22:45:46 +0800

    20201203读取首行IP

--- print_ip.c
+++ print_ip.c
@@ -5,8 +5,8 @@
 #define IP_LENGTH 15
 #define DATE_LENGTH 20
 #define HITTIMES_LENGTH 5
-char * get_IP(char * ch, char * ip_ptr);
 
+//创建结构体，记录攻击ip，生成日期，命中数
 struct IP_info
 {
     char ip[IP_LENGTH];
@@ -15,12 +15,13 @@ struct IP_info
 };
 
 int main(void)
-{
-    struct IP_info first_line;
-    char temp_receive_line[MAX];
-    FILE * fp;
-    char * ip_ptr;
+{   
+    struct IP_info first_line;     //创建一个结构体变量，用于记录从 temp_receive_line 数组传递过来的数据
+    char temp_receive_line[MAX];   //创建一个临时数组，用于接收文件流的数据
+    FILE * fp;                     //创建一个指针，用于读取文件
+    char * ip_ptr;                 //创建一个指针，用于将读取的IP写入结构体成员变量 first_line.ip
 
+    //打开文件
     fp = fopen("file.txt", "r");
     if(fp == NULL)
     {
@@ -28,6 +29,7 @@ int main(void)
 	return(-1);
     }
 
+    //读取字符串并输出
     if( fgets(temp_receive_line, MAX, fp) != NULL )
     {
 	/* 向标准输出 stdout 写入内容 */
@@ -36,8 +38,8 @@ int main(void)
 
     fclose(fp);
 
-    ip_ptr = first_line.ip;
-    get_IP(temp_receive_line, ip_ptr);
-    puts(first_line.ip);
+    ip_ptr = first_line.ip;      
+    get_IP(temp_receive_line, ip_ptr); //调用 get_IP.c 中的 get_IP 函数，将读取的IP写入结构体成员变量
+    puts(first_line.ip); 
 
 }
