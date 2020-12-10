2df17158a9da8ded1720efd9cbfef66848e65419
zsx0728 <zsx0728@163.com>
2020-12-10 10:43:37 +0800

    完善注释

--- print_ip.c
+++ print_ip.c
@@ -2,11 +2,11 @@
 #include<string.h>
 #include "get_Info.c"
 
-#define LINE_MAX_BYTE 150
-#define IP_LENGTH 16
-#define DATE_LENGTH 20
-#define HITTIMES_LENGTH 6
-#define MAX_LINE 2048
+#define LINE_MAX_BYTE 150     //待处理的文件的每行最大字节数
+#define IP_LENGTH 16          //IP长度
+#define DATE_LENGTH 20        //日期长度
+#define HITTIMES_LENGTH 6     //命中数长度
+#define MAX_LINE 2048         //待处理文件最大行数
 
 //创建结构体，记录攻击ip，生成日期，命中数
 struct IP_info
@@ -18,17 +18,17 @@ struct IP_info
 
 int main(void)
 {   
-    struct IP_info oldfile_Lines[MAX_LINE];     //创建一个结构体变量，用于记录从 temp_receive_line 数组传递过来的旧文件数据
-    struct IP_info newfile_Lines[MAX_LINE];     //创建一个结构体变量，用于记录从 temp_receive_line 数组传递过来的新文件数据
+    struct IP_info oldfile_Lines[MAX_LINE];        //创建一个结构体变量，用于记录从 temp_receive_line 数组传递过来的旧文件数据
+    struct IP_info newfile_Lines[MAX_LINE];        //创建一个结构体变量，用于记录从 temp_receive_line 数组传递过来的新文件数据
     char temp_receive_line[MAX_LINE][LINE_MAX_BYTE]={0};   //创建一个临时数组，用于接收文件流的数据
-    FILE * readfile_fp;                     //创建一个指针，用于读取文件
-    FILE * writefile_fp;                     //创建一个指针，用于写入文件
-    char * ip_ptr;                 //创建一个指针，用于将读取的IP写入结构体成员变量 oldfile_Lines[MAX_LINE].ip
-    char * date_ptr;
-    char * hittimes_ptr;
-    int count=0, oldfile_lines=0, newfile_lines=0;
+    FILE * readfile_fp;                            //创建一个指针，用于读取待处理文件
+    FILE * writefile_fp;                           //创建一个指针，用于将处理结果写入文件
+    char * ip_ptr;                                 //创建一个指针，用于将从临时数组temp_receive_line读取的IP写入结构体成员变量Ip
+    char * date_ptr;                               //创建一个指针，用于将从临时数组temp_receive_line读取的日期写入结构体成员变量Date
+    char * hittimes_ptr;                           //创建一个指针，用于将从临时数组temp_receive_line读取的命中数写入结构体成员变量HitTimes
+    int count=0, oldfile_lines=0, newfile_lines=0; //count用于循环计数，oldfile_lines记录旧文件行数，newfile_lines记录新文件行数
  
-    //读取旧文件的IP，日期，命中数
+    //第一步，读取旧文件的IP，日期，命中数
     //打开文件
     readfile_fp = fopen("old.txt", "r");
     if(readfile_fp == NULL)
@@ -58,7 +58,7 @@ int main(void)
     fclose(readfile_fp);
     oldfile_lines = count;
 
-    //读取新文件的IP，日期，命中数
+    //第二步，读取新文件的IP，日期，命中数
     count=0;
 
     //打开文件
@@ -90,7 +90,7 @@ int main(void)
     fclose(readfile_fp);
     newfile_lines = count;
 
-    //暴力比较法，循环提取旧文件的一行，与新文件逐行比较，如果IP相同，写入文件 /tmp/final.txt。
+    //第三步，暴力比较法，循环提取旧文件的一行，与新文件逐行比较，如果IP和命中数相同，写入文件
     int count_oldfile=0, count_newfile=0;
     struct IP_info * old_ptr;
     struct IP_info * new_ptr;
@@ -103,10 +103,11 @@ int main(void)
 	{
 	    if ( (strcmp(old_ptr->Ip, new_ptr->Ip) == 0) && (strcmp(old_ptr->HitTimes, new_ptr->HitTimes) == 0) )
 	    {
+		//将新旧文件相同的IP和命中数写入文件
                 writefile_fp = fopen("/tmp/final.txt", "a+");
 		fprintf(writefile_fp, new_ptr->Ip);
-		//fprintf(writefile_fp," ");
-		//fprintf(writefile_fp,old_ptr->HitTimes);
+		fprintf(writefile_fp," ");
+		fprintf(writefile_fp,old_ptr->HitTimes);
 		fprintf(writefile_fp,"\n");
 		fclose(writefile_fp);
 		break;
